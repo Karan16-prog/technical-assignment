100b036677d1094c40d555f41a8fc37988404dd4
Karan16-prog <karan.s1606@gmail.com>
2021-12-02 11:39:01 +0530

    made mui form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {Typography, Card, Grid, TextField, Button} from '@material-ui/core';
+import { CardContent } from '@mui/material';
 
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Hello</p>
+     <Card style={{maxWidth:500, margin:"0 auto", padding:"25px 10px"}}>
+      <CardContent>
+        <Typography gutterBottom variant='h5'>Please Fill the Form</Typography>
+        <form>
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item>
+            <TextField label="First Name" placeholder="Enter First Name" variant="outlined" fullWidth required/>
+          </Grid>
+          <Grid xs={12} sm={6} item>
+            <TextField label="Last Name" placeholder="Enter Last Name" variant="outlined" fullWidth required/>
+          </Grid>
+          <Grid xs={12} item>
+            <TextField type="email" label="Email" placeholder="Enter Email" variant="outlined" fullWidth required/>
+          </Grid>
+          <Grid xs={12} item>
+            <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+          </Grid>
+        </Grid>
+        </form>
+      </CardContent>
+     </Card>
     </div>
   );
 }
 
 export default App;
+
